d5aef737d1257c04f4fa8b963a0ce9bbaa54f2f2
Debasish Das <das31debasish@gmail.com>
2024-06-19 17:16:47 +0530

    m

--- main.cpp
+++ main.cpp
@@ -1,6 +1,55 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#include "Model/RubiksCube3dArrayRep.cpp"
 
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+RubiksCube3dArray obj3DArray;
+
+
+
+//obj3DArray.l();
+
+//    obj3DArray.u();
+//   if(obj3DArray.isSolved()) {
+//       cout<<"YES \n";
+//   }else{
+//       cout<<"No\n";
+//
+//   }
+//
+//    obj3DArray.print();
+//
+//    obj3DArray.uPrime();
+//    if(obj3DArray.isSolved()) {
+//        cout<<"YES \n";
+//    }else{
+//        cout<<"No\n";
+//
+//    }
+//
+//
+//    obj3DArray.print();
+//
+
+vector<RubiksCube33::MOVE>movesForSuffeling =obj3DArray.randomSuffleCube(5);
+
+for(auto move:movesForSuffeling){ // getting idx of MOVE
+    cout<<obj3DArray.getMove(move)<<" "; // return  actual what idx of MOVE is performed
+}
+cout<<endl;
+    if(obj3DArray.isSolved()) {
+        cout<<"YES \n";
+    }else{
+        cout<<"No\n";
+
+    }
+
+    obj3DArray.print();
+
+
+
+
+
     return 0;
 }
